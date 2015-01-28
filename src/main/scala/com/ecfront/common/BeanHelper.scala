4bfdd0e9f860267b971a1d4c7afdd1a5b6c1cdd2
gudaoxuri <net@sunisle.org>
2015-01-28 15:17:06 +0800

    Add Bean Functions.

--- src/main/scala/com/ecfront/common/BeanHelper.scala
+++ src/main/scala/com/ecfront/common/BeanHelper.scala
@@ -2,10 +2,15 @@ package com.ecfront.common
 
 import org.apache.commons.beanutils.BeanUtilsBean
 
+import scala.annotation.{StaticAnnotation, tailrec}
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.runtime.universe._
+
 /**
  * Bean操作辅助类
  */
 object BeanHelper {
+
   private val copyPropertiesAdapter = new NullAwareBeanUtilsBean
 
   /**
@@ -15,6 +20,75 @@ object BeanHelper {
    */
   def copyProperties(dest: AnyRef, orig: AnyRef) = copyPropertiesAdapter.copyProperties(dest, orig)
 
+  /**
+   * 获取Bean的字段名称及类型
+   * @param beanClazz 目标Bean类型
+   * @param filterNames 要过滤的名称
+   * @param filterAnnotations 要过滤的注解
+   */
+  def getFields(beanClazz: Class[_], filterNames: Seq[String] = Seq(), filterAnnotations: Seq[Class[_ <: StaticAnnotation]] = Seq(classOf[Ignore])): Map[String, String] = {
+    val fields = collection.mutable.Map[String, String]()
+    val filters = ArrayBuffer[String]()
+    if (filterAnnotations != null && filterAnnotations.nonEmpty) {
+      getFieldAnnotations(filters, beanClazz, filterAnnotations)
+    }
+    val rm = scala.reflect.runtime.currentMirror
+    rm.classSymbol(beanClazz).toType.members.collect {
+      case m: MethodSymbol if m.isGetter && m.isPublic
+        && (filterNames == null || filterNames.isEmpty || !filterNames.contains(m.name))
+      =>
+        fields += (m.name.toString.trim -> m.returnType.toString.trim)
+    }
+    fields.filter(f => !filters.contains(f._1)).toMap
+  }
+
+  /**
+   * 获取Bean中字段的值
+   * @param bean 目标Bean
+   * @param filterNames 要过滤的名称
+   * @param filterAnnotations 要过滤的注解
+   */
+  def getValues(bean: AnyRef, filterNames: Seq[String] = Seq(), filterAnnotations: Seq[Class[_ <: StaticAnnotation]] = Seq(classOf[Ignore])): Map[String, Any] = {
+    if (bean == null) {
+      throw new IllegalArgumentException("No bean specified")
+    }
+    val fields = collection.mutable.Map[String, Any]()
+    val filters = ArrayBuffer[String]()
+    if (filterAnnotations != null && filterAnnotations.nonEmpty) {
+      getFieldAnnotations(filters, bean.getClass, filterAnnotations)
+    }
+    val rm = scala.reflect.runtime.currentMirror
+    val instanceMirror = rm.reflect(bean)
+    rm.classSymbol(bean.getClass).toType.members.collect {
+      case m: MethodSymbol if m.isGetter && m.isPublic
+        && (filterNames == null || filterNames.isEmpty || !filterNames.contains(m.name))
+      =>
+        fields += (m.name.toString.trim -> instanceMirror.reflectMethod(m).apply())
+    }
+    fields.filter(f => !filters.contains(f._1)).toMap
+  }
+
+  /**
+   * 递归获取带指定注解的字段
+   * @param container 存放字段容器
+   * @param beanClazz 目标Bean
+   * @param annotations 要过滤的注解
+   */
+  @tailrec
+  private def getFieldAnnotations(container: ArrayBuffer[String], beanClazz: Class[_], annotations: Seq[Class[_ <: StaticAnnotation]]) {
+    val rm = scala.reflect.runtime.currentMirror
+    rm.classSymbol(beanClazz).toType.members.collect {
+      case m if !m.isMethod &&
+        annotations.exists(ann => m.annotations.exists(ann.getName == _.toString)) =>
+        container += m.name.toString.trim
+    }
+    beanClazz.getGenericSuperclass match {
+      case c: Class[_] =>
+        if (c != classOf[Object]) {
+          getFieldAnnotations(container, c, annotations)
+        }
+    }
+  }
 }
 
 private class NullAwareBeanUtilsBean extends BeanUtilsBean {
@@ -24,3 +98,6 @@ private class NullAwareBeanUtilsBean extends BeanUtilsBean {
     }
   }
 }
+
+@scala.annotation.meta.field
+class Ignore extends scala.annotation.StaticAnnotation
---
+++ src/test/scala/com/ecfront/common/CommonSpec.scala
@@ -0,0 +1,59 @@
+package com.ecfront.common
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.scalatest._
+
+import scala.beans.BeanProperty
+
+class CommonSpec extends FunSuite {
+
+  test("配置测试") {
+    val value1 = ConfigHelper.init[JsonNode](this.getClass.getResource("/config.json").getPath).get.path("loglevel").asText()
+    assert(value1 == "DEBUG")
+    val value2 = ConfigHelper.init[Config](this.getClass.getResource("/config.json").getPath, classOf[Config]).get
+    assert(value2.loglevel == "DEBUG")
+    val value3 = ConfigHelper.init[Config](getClass.getResource("/").getPath + "config2.json", classOf[Config]).get
+    assert(value3.loglevel == "INFO")
+  }
+
+  test("Json测试") {
+    val a = JsonHelper.toJson(Config("aaa"))
+    val b = JsonHelper.toJson( """{"a_key":"a_val"}""")
+    JsonHelper.toJson( """{"a_key":"a_val"}""")
+    print(JsonHelper.toJsonString(JsonHelper.createObjectNode().set("", JsonHelper.createObjectNode().put("a_key", "a_val"))))
+  }
+
+  test("Bean测试") {
+    val fields = BeanHelper.getFields(classOf[TestModel])
+    assert(fields.size == 3)
+    assert(fields("id") == "String")
+    assert(fields("name") == "String")
+    assert(fields("bool") == "Boolean")
+
+    val model = TestModel("张三", true, 14)
+    model.id = "id001"
+    val values = BeanHelper.getValues(model)
+    assert(values.size == 3)
+    assert(values("id") == "id001")
+    assert(values("name") == "张三")
+    assert(values("bool") == true)
+  }
+}
+
+
+case class TestModel(
+                      @BeanProperty var name: String,
+                      @BeanProperty var bool: Boolean,
+                      @BeanProperty @Ignore var age: Int
+                      ) extends IdModel {
+}
+
+abstract class IdModel {
+  @BeanProperty var id: String = _
+  @BeanProperty
+  @Ignore var title: String = _
+}
+
+
+case class Config(loglevel: String)
+
