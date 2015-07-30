4ff0c6a3e84fe5d14b52b62d857c962a8c5d3ec9
Nestor <nestor@thinkb2c.com>
2015-07-30 09:54:14 +0800

    Add ReqRespVO helper functions.

--- src/test/scala/com/ecfront/common/ReqRespVOSpec.scala
+++ src/test/scala/com/ecfront/common/ReqRespVOSpec.scala
@@ -13,6 +13,8 @@ class ReqRespVOSpec extends FunSuite {
     assert(t3.code == StandardCode.SUCCESS)
     val t4 = test4()
     assert(t4.code == StandardCode.BAD_REQUEST)
+    val t5 = test5()
+    assert(t5.code == Resp.CUSTOM_CODE_PREFIX + "4212")
   }
 
   def test1(): Resp[Some_Model] = {
@@ -20,7 +22,7 @@ class ReqRespVOSpec extends FunSuite {
   }
 
   def test2(): Resp[Some_Model] = {
-    Resp.fail(StandardCode.BAD_REQUEST, "错误")
+    Resp.badRequest("错误")
   }
 
   def test3(): Resp[Some_Model] = {
@@ -31,5 +33,8 @@ class ReqRespVOSpec extends FunSuite {
     test2()
   }
 
+  def test5(): Resp[Some_VO] = {
+    Resp.customFail("4212", "错误")
+  }
 }
 
