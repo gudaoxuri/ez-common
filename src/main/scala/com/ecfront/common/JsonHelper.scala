08b84aecbf47d5f2aafc7ddde46bdfbf6d3a87fc
gudaoxuri <i@sunisle.org>
2015-12-16 13:58:47 +0800

    DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES

--- src/main/scala/com/ecfront/common/JsonHelper.scala
+++ src/main/scala/com/ecfront/common/JsonHelper.scala
@@ -1,33 +1,36 @@
 package com.ecfront.common
 
 import com.fasterxml.jackson.databind.node.{ArrayNode, ObjectNode}
-import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import com.fasterxml.jackson.databind.{DeserializationFeature, JsonNode, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
 /**
- * Scala版本的Json辅助类<br/>
- * 使用<i>jackson-module-scala</i>封装
- */
+  * Scala版本的Json辅助类<br/>
+  * 使用<i>jackson-module-scala</i>封装
+  */
 object JsonHelper {
 
   private val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
   /**
-   * object 转 json字符串
-   * @param obj object
-   * @return json字符串
-   */
+    * object 转 json字符串
+    *
+    * @param obj object
+    * @return json字符串
+    */
   def toJsonString(obj: Any): String = {
     mapper.writeValueAsString(obj)
   }
 
   /**
-   * object 转 json
-   * @param obj object
-   * @return json
-   */
+    * object 转 json
+    *
+    * @param obj object
+    * @return json
+    */
   def toJson(obj: Any): JsonNode = {
     obj match {
       case o: String => mapper.readTree(o)
@@ -36,10 +39,11 @@ object JsonHelper {
   }
 
   /**
-   * json或string 转 object
-   * @param obj json或string
-   * @return object
-   */
+    * json或string 转 object
+    *
+    * @param obj json或string
+    * @return object
+    */
   def toObject[E](obj: Any, clazz: Class[E]): E = {
     obj match {
       case o: String =>
@@ -71,8 +75,8 @@ object JsonHelper {
   }
 
   /**
-   * json或string 转 generic object
-   */
+    * json或string 转 generic object
+    */
   def toGenericObject[E: Manifest](obj: Any): E = {
     obj match {
       case o: String => mapper.readValue[E](o)
