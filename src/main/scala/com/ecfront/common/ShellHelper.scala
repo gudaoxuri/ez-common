c7929a95e32dc1dad41595e89ef986e955e31af5
gudaoxuri <i@sunisle.org>
2016-02-29 10:31:00 +0800

    Add some fun by ShellHelper.

--- src/main/scala/com/ecfront/common/ShellHelper.scala
+++ src/main/scala/com/ecfront/common/ShellHelper.scala
@@ -5,9 +5,11 @@ import java.util.concurrent.{CountDownLatch, Executors}
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
+import scala.concurrent.{Future, Promise}
+
 /**
- * Shell辅助类
- */
+  * Shell辅助类
+  */
 case class ShellHelper(command: String, reportHandler: ReportHandler = null, taskId: String = "", successFlag: String = null, progressFlag: String = null) extends LazyLogging {
 
   private val result = new StringBuffer
@@ -16,7 +18,7 @@ case class ShellHelper(command: String, reportHandler: ReportHandler = null, tas
   def execute(rr: Boolean): Unit = {
     returnResult = rr
     try {
-      val process = Runtime.getRuntime.exec(Array[String]("/bin/sh", "-c", command.replaceAll("\r\n\\s*"," ").replaceAll("\n\\s*"," ")))
+      val process = Runtime.getRuntime.exec(Array[String]("/bin/sh", "-c", command.replaceAll("\r\n\\s*", " ").replaceAll("\n\\s*", " ")))
       if (process != null) {
         val errorGobbler = new StreamGobbler(process.getErrorStream)
         val outputGobbler = new StreamGobbler(process.getInputStream)
@@ -58,8 +60,8 @@ case class ShellHelper(command: String, reportHandler: ReportHandler = null, tas
   }
 
   /**
-   * 输出处理
-   */
+    * 输出处理
+    */
   private class StreamGobbler(is: InputStream) extends java.util.concurrent.Callable[Boolean] {
 
     def call: Boolean = {
@@ -133,13 +135,13 @@ case class ShellHelper(command: String, reportHandler: ReportHandler = null, tas
 object ShellHelper {
 
   /**
-   * @param command     shell命令
-   * @param reportHandler 任务报告实例
-   * @param taskId        任务ID
-   * @param successFlag   成功标识，只要捕捉到此标识就视为成功
-   * @param progressFlag  进度标识，只要捕捉到此标识就更新进度， 格式为 <progressFlag>空格<progress>,如： progress 40
-   * @param returnResult  是否返回结果
-   */
+    * @param command       shell命令
+    * @param reportHandler 任务报告实例
+    * @param taskId        任务ID
+    * @param successFlag   成功标识，只要捕捉到此标识就视为成功
+    * @param progressFlag  进度标识，只要捕捉到此标识就更新进度， 格式为 <progressFlag>空格<progress>,如： progress 40
+    * @param returnResult  是否返回结果
+    */
   def async(command: String, reportHandler: ReportHandler, taskId: String, successFlag: String, progressFlag: String, returnResult: Boolean): Unit = {
     new ShellHelper(command, reportHandler, taskId, successFlag, progressFlag).execute(returnResult)
   }
@@ -154,6 +156,64 @@ object ShellHelper {
 
   def async(command: String): Unit = {
     new ShellHelper(command).execute(rr = false)
+  }
+
+  def async(command: String, successFlag: String, returnResult: Boolean): Future[Resp[String]] = {
+    val p = Promise[Resp[String]]()
+    var isSuccess = false
+    var resp: Resp[String] = null
+    new ShellHelper(command, new ReportHandler {
+
+      override def progress(taskId: String, progress: Int): Unit = {}
+
+      override def success(taskId: String): Unit = {
+        isSuccess = true
+      }
+
+      override def complete(taskId: String, result: String): Unit = {
+        if (successFlag != null && !isSuccess) {
+          p.success(Resp.serverError("Success Flag NOT found"))
+        } else if (resp == null) {
+          p.success(Resp.success(result))
+        } else {
+          p.success(resp)
+        }
+      }
+
+      override def fail(taskId: String, message: String): Unit = {
+        resp = Resp.serverError(message)
+      }
+    }, System.nanoTime() + "", successFlag).execute(returnResult)
+    p.future
+  }
+
+  def sync(command: String, successFlag: String, returnResult: Boolean): Resp[String] = {
+    val completeFlag = new CountDownLatch(1)
+    var isSuccess = false
+    var resp: Resp[String] = null
+    new ShellHelper(command, new ReportHandler {
+
+      override def progress(taskId: String, progress: Int): Unit = {}
+
+      override def success(taskId: String): Unit = {
+        isSuccess = true
+      }
+
+      override def complete(taskId: String, result: String): Unit = {
+        if (successFlag != null && !isSuccess) {
+          resp = Resp.serverError("Success Flag NOT found")
+        } else if (resp == null) {
+          resp = Resp.success(result)
+        }
+        completeFlag.countDown()
+      }
+
+      override def fail(taskId: String, message: String): Unit = {
+        resp = Resp.serverError(message)
+      }
+    }, System.nanoTime() + "", successFlag).execute(returnResult)
+    completeFlag.await()
+    resp
   }
 
   def sync(command: String, returnResult: Boolean = false): String = {
@@ -182,38 +242,38 @@ object ShellHelper {
 }
 
 /**
- * 任务报告接口
- */
+  * 任务报告接口
+  */
 trait ReportHandler {
   /**
-   * 成功
-   *
-   * @param taskId 任务ID
-   */
+    * 成功
+    *
+    * @param taskId 任务ID
+    */
   def success(taskId: String)
 
   /**
-   * 失败
-   *
-   * @param taskId  任务ID
-   * @param message 失败原因描述
-   */
+    * 失败
+    *
+    * @param taskId  任务ID
+    * @param message 失败原因描述
+    */
   def fail(taskId: String, message: String)
 
   /**
-   * 进度
-   *
-   * @param taskId   任务ID
-   * @param progress 0-100
-   */
+    * 进度
+    *
+    * @param taskId   任务ID
+    * @param progress 0-100
+    */
   def progress(taskId: String, progress: Int)
 
   /**
-   * 完成
-   *
-   * @param taskId   任务ID
-   * @param result 结果
-   */
+    * 完成
+    *
+    * @param taskId 任务ID
+    * @param result 结果
+    */
   def complete(taskId: String, result: String)
 
 }
