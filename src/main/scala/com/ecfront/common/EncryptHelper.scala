4287eefbf49232bc783d165873b9ffeda747f346
gudaoxuri <i@sunisle.org>
2016-07-17 10:13:15 +0800

    Add bcrypt support.

--- src/main/scala/com/ecfront/common/EncryptHelper.scala
+++ src/main/scala/com/ecfront/common/EncryptHelper.scala
@@ -2,26 +2,51 @@ package com.ecfront.common
 
 import java.security.MessageDigest
 
+import com.github.t3hnar.bcrypt._
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
 object EncryptHelper extends LazyLogging {
 
   /**
-   * 加密
-   *
-   * @param strSrc 原始值
-   * @param algorithm 加密算法
-   * @return 加密后的值
-   */
+    * 加密
+    *
+    * @param strSrc    原始值
+    * @param algorithm 加密算法
+    * @return 加密后的值
+    */
   def encrypt(strSrc: String, algorithm: String = "SHA-256"): String = {
-    val md = MessageDigest.getInstance(algorithm)
-    md.digest(strSrc.getBytes)
-      .foldLeft("")((s: String, b: Byte) => s +
-      Character.forDigit((b & 0xf0) >> 4, 16) +
-      Character.forDigit(b & 0x0f, 16))
+    algorithm.toLowerCase() match {
+      case "bcrypt" =>
+        strSrc.bcrypt
+      case _ =>
+        val md = MessageDigest.getInstance(algorithm)
+        md.digest(strSrc.getBytes)
+          .foldLeft("")((s: String, b: Byte) => s +
+            Character.forDigit((b & 0xf0) >> 4, 16) +
+            Character.forDigit(b & 0x0f, 16))
+    }
   }
 
-  def bytes2hex(bytes: Array[Byte], sep: Option[String] = None): String = {
+  /**
+    * 验证
+    *
+    * 对于bcrypt之类默认使用随机slat的加密算法必须使用此方法验证
+    *
+    * @param strSrc       原始值
+    * @param strEncrypted 加密后的值
+    * @param algorithm    加密算法
+    * @return 是否匹配
+    */
+  def validate(strSrc: String, strEncrypted: String, algorithm: String = "SHA-256"): Boolean = {
+    algorithm.toLowerCase() match {
+      case "bcrypt" =>
+        strSrc.isBcrypted(strEncrypted)
+      case _ =>
+        encrypt(strSrc, algorithm) == strEncrypted
+    }
+  }
+
+  private def bytes2hex(bytes: Array[Byte], sep: Option[String] = None): String = {
     sep match {
       case None => bytes.map("%02x".format(_)).mkString
       case _ => bytes.map("%02x".format(_)).mkString(sep.get)
--- src/test/scala/com/ecfront/common/EncryptHelperSpec.scala
+++ src/test/scala/com/ecfront/common/EncryptHelperSpec.scala
@@ -6,6 +6,8 @@ class EncryptHelperSpec extends FunSuite {
 
   test("加密测试") {
     assert(EncryptHelper.encrypt("gudaoxuri") == "70C0CC2B7BF8A8EBCD7B59C49DDDA9A1E551122BA5D7AB3B7B02141D4CE4C626".toLowerCase)
+    assert(EncryptHelper.validate("gudaoxuri", EncryptHelper.encrypt("gudaoxuri")))
+    assert(EncryptHelper.validate("password", EncryptHelper.encrypt("password", "bcrypt"), "bcrypt"))
   }
 
 }
